e65b1635b542fa953c2c66c6ab792ab04d608083
Yipmong Said <Syipmong5@gmail.com>
2026-05-02 16:41:36 +0100

    fix: comment out unused PostgreSQL connection string in database.py

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./my_database.db"
+# """postgres://d046b351d083596ca674b5714ca16d50b524dc0ad5f05c03371629bfb6ae42ec:sk_zP7VQvhdvB4pdK-cB_0hp@db.prisma.io:5432/postgres?sslmode=require"""
 
 engine = create_engine(
     DATABASE_URL,

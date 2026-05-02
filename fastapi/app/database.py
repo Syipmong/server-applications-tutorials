5921a319762b8f4aaffaadf5991b98446e3f13bf
Yipmong Said <Syipmong5@gmail.com>
2026-05-02 17:17:21 +0100

    fix: refactor database connection and user management in FastAPI application

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./my_database.db"
-# """postgres://d046b351d083596ca674b5714ca16d50b524dc0ad5f05c03371629bfb6ae42ec:sk_zP7VQvhdvB4pdK-cB_0hp@db.prisma.io:5432/postgres?sslmode=require"""
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
+    # connect_args={
+    #     "check_same_thread": False
+    # }
 )
 
 sessionLocal = sessionmaker(autoflush=False, bind=engine, expire_on_commit=False)
--- fastapi/app/routers/users.py
+++ fastapi/app/routers/users.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
+from app.database import sessionLocal
+from app import models
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class UserCreate(BaseModel):
@@ -11,20 +21,30 @@ class UserCreate(BaseModel):
     age: int
     is_active: bool = True
 
-user_list = []
 
 @router.get('/users/{username}')
-def get_user(username: str):
-    for user_obj in user_list:
-        if username == user_obj.username:
+def get_user(username: str, db: Session = Depends(get_db)):
+    for user_obj in db.query(models.User).filter(models.User.username == username).first():
+        if user_obj:
             return {'message': f'{username} found in the list'}
     raise HTTPException(status_code=404, detail="user not found")
 
 @router.post('/users/')
-def create_user(user: UserCreate):
-    user_list.append(user)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(
+        username = user.name,
+        age = user.age,
+        email = user.email,
+        phone = user.phone,
+        is_active = user.is_active
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
     return {"message": "user created successfully!", "user_data": user}
     
 @router.get('/users')
-def get_users():
-    return {'users': user_list}
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return {'users': users}
